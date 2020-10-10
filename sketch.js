d6d9813f5cab365f80da839824f3dae43928e88e
Rehaan27 <72610865+Rehaan27@users.noreply.github.com>
2020-10-10 12:36:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,99 @@
+
+var monkey , monkey_running, gameover;
+var banana ,bananaImage, obstacle, obstacleImage;
+var FoodGroup, obstacleGroup, survivalTime, r;
+var score, gamestate;
+
+function preload(){
+  
+  
+  monkey_running =loadAnimation("sprite_0.png" ,"sprite_1.png" ,"sprite_2.png","sprite_3.png" ,"sprite_4.png" ,"sprite_5.png" ,"sprite_6.png","sprite_7.png","sprite_8.png");
+  
+  bananaImage = loadImage("banana.png");
+  obstacleImage = loadImage("obstacle.png");
+
+ 
+}
+
+function setup() {
+  
+createCanvas(400, 400);  
+  
+monkey = createSprite(80,315, 20, 20);
+monkey.addAnimation("moving",monkey_running);
+monkey.scale = 0.1;
+  
+ground = createSprite(400, 350, 900, 10);
+ground.velocityX = -5;
+ground.x = ground.width/2;
+  
+survivalTime = 0;
+  
+}
+
+
+function draw() {
+background("green");
+  
+  if(ground.x<0){
+    ground.x = ground.width/2;
+  }
+  
+  stroke("white");
+  textSize(20);
+  fill("white");
+  
+  survivalTime = Math.ceil(frameCount/frameRate());
+  
+  text("Survival Time:"+survivalTime, 100, 150);
+  
+  if(gamestate = "play"){
+    
+  if(keyDown("space")&&monkey.y>312){
+    monkey.velocityY = monkey.velocityY-15; 
+  }
+ 
+  monkey.debug = true; 
+  monkey.velocityY = monkey.velocityY+0.8; 
+  obstacleGroup = new Group(); 
+  FoodGroup = new Group();
+  spawnStones(); 
+  spawnFruits();
+  
+  if(obstacleGroup.isTouching(monkey)){
+    gamestate = "end";
+  }}
+  
+  
+    monkey.collide(ground);
+  
+  drawSprites();
+}
+
+function spawnStones(){
+  
+ if(frameCount%100===0){
+   
+   obstacle = createSprite(400, 315, 50, 50);
+   obstacle.addImage("stone",obstacleImage);
+   obstacle.scale = 0.2;;
+   obstacle.velocityX = -5
+   obstacleGroup.add(obstacle);
+ } 
+  
+}
+
+ function spawnFruits(){
+  
+ if(frameCount%60===0){
+  
+   banana = createSprite(400, 250, 40, 40);
+   banana.addImage(bananaImage);
+   banana.scale = 0.1;
+   r = Math.round(1,2);   
+   banana.y = Math.round(random(250, 200));
+   banana.velocityX = -5;
+   banana.lifetime = 90;
+   FoodGroup.add(banana);
+  } 
+}
